10808badfe97c8b0f21774b01faf989f59222497
KMSunilkumar <119863275+KMSunilkumar@users.noreply.github.com>
2023-03-17 14:58:37 +0530

    Add files via upload

---
+++ Library-Management-System/src/main/java/springboot/project/Library/Management/System/model/user.java
@@ -0,0 +1,28 @@
+package springboot.project.Library.Management.System.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GeneratedType;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name="sunil_user")
+public class user {
+	@Id
+	
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+     private int id;
+	
+	@column(name ="first_name", nullable= false)
+     private String firstName;
+	
+	@column(name = "last_name")
+     private String lastname;
+	
+	@column(name = "email")
+     private String email;
+	
+}
